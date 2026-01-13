05a5d087e1bcb9f25a4e9251942dd3be4801ebbc
emweston <emweston@student.fullsail.edu>
2026-01-13 12:33:18 -0800

    Verifying functionality: InteractionController
    Verify InteractionController runs and handles input.

--- Assets/Scripts/Interaction/InteractionController.cs
+++ Assets/Scripts/Interaction/InteractionController.cs
@@ -5,12 +5,15 @@ public class InteractionController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Hey, y'all, InteractionController is running. Woohoo!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("InteractionController detected a P key. Woohoo!");
+        }
     }
 }
